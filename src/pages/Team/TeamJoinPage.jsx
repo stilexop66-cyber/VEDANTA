175711c8c15f88cb27e5891488fd71b18499bbd5
stilexop66-cyber <stilexop66@gmail.com>
2026-06-04 16:56:26 +0530

    Update Join Our Team page

--- src/pages/Team/TeamJoinPage.jsx
+++ src/pages/Team/TeamJoinPage.jsx
@@ -1,12 +1,16 @@
-﻿import Footer from "../../components/layout/Footer";
+﻿/* eslint-disable no-unused-vars */
+import React from "react";
+import Footer from "../../components/layout/Footer";
 import TeamJoinForm from "./TeamJoinForm";
 import OurTeamHero from "./OurTeamHero";
 
 export default function TeamJoinPage() {
   return (
     <div className="bg-[#303030] min-h-screen flex flex-col">
+      {/* Hero Section */}
       <OurTeamHero />
 
+      {/* Form Section */}
       <div className="max-w-7xl mx-auto py-12 px-6 flex-grow">
         <div className="text-center mb-10">
           <h1 className="text-3xl md:text-5xl font-bold font-serif mb-4 text-[#e0e0e0]">
