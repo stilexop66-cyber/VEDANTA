299e08a6b096ce656c64661bcb3b719efd2a529d
stilexop66-cyber <stilexop66@gmail.com>
2026-06-13 22:17:38 +0530

    whatsappicon

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,23 +1,64 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "../pages/Home/Home";
 import PracticeAreas from "../pages/PracticeAreas/PracticeAreas";
 import Team from "../pages/Team/Team";
 import TeamJoinPage from "../pages/Team/TeamJoinPage";
 import Contact from "../pages/Contact/Contact";
 
+import WhatsAppButton from "../components/layout/WhatsAppButton";
+
+
 export default function AppRoutes() {
+
   return (
+
     <BrowserRouter>
+
+
+      <WhatsAppButton />
+
+
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/practice-areas" element={<PracticeAreas />} />
-        <Route path="/team" element={<Team />} />
-        <Route path="/join-our-team" element={<TeamJoinPage />} />
-        <Route path="/contact" element={<Contact />} />
-        {/* We will add Contact route here later */}
+
+
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+
+        <Route
+          path="/practice-areas"
+          element={<PracticeAreas />}
+        />
+
+
+        <Route
+          path="/team"
+          element={<Team />}
+        />
+
+
+        <Route
+          path="/join-our-team"
+          element={<TeamJoinPage />}
+        />
+
+
+        <Route
+          path="/contact"
+          element={<Contact />}
+        />
+
+
       </Routes>
+
+
     </BrowserRouter>
+
   );
+
 }
