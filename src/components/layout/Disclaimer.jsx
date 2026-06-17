47b7595e1925fedc712d3e0a654549bcd0040580
stilexop66-cyber <stilexop66@gmail.com>
2026-06-17 14:20:23 +0530

    popup complete

--- src/components/layout/Disclaimer.jsx
+++ src/components/layout/Disclaimer.jsx
@@ -40,33 +40,12 @@ const Disclaimer = () => {
 
 
         <p>
-
-        As per the rules of the Bar Council of India,
-        we are not permitted to solicit work and advertise.
-        By clicking "I Agree" you confirm the following:
-
+In accordance with the rules of the Bar Council of India, we are not permitted to solicit work or advertise our services. By clicking the “I Agree” button, you acknowledge and confirm that no advertisement, solicitation, personal communication, invitation, or inducement of any kind has been made by us or any member of our firm to attract work through this website; that you are accessing this site solely to understand our firm’s offerings for your own information and use; that any specific information provided is at your request; that any material downloaded or information obtained from this website is entirely at your own volition; and that your use of this site does not create any lawyer-client relationship. The content on this website is intended strictly for general informational purposes and should not be interpreted as legal advice, solicitation, or an advertisement. We accept no liability for any consequences arising from any action taken or decision made by you in reliance on the information or materials available here, and you are strongly advised to seek independent legal advice for any legal matter.
         </p>
 
 
 
-        <ol>
-
-          <li>
-          No advertisement or solicitation has been made.
-          </li>
-
-
-          <li>
-          You are visiting this website voluntarily.
-          </li>
-
-
-          <li>
-          Information is only for general purposes.
-          </li>
-
-
-        </ol>
+       
 
 
 
